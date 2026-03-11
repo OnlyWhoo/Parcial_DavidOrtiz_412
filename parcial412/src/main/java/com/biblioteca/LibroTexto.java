468e37f59e4f0372f111c2a77145beeb0accdf14
Juan D Ortiz <jdavorgor@gmail.com>
2026-03-10 20:07:24 -0500

    Crear clase LibroTexto heredada de Libro

---
+++ parcial412/src/main/java/com/biblioteca/LibroTexto.java
@@ -0,0 +1,33 @@
+package com.biblioteca;
+public class LibroTexto extends Libro {
+
+    private String curso;
+
+    public LibroTexto() {
+        super();
+        this.curso = "";
+    }
+
+    // Constructor
+    public LibroTexto(String titulo, String autor, int numeroEjemplares, int numeroEjemplaresPrestados, String curso) {
+        super(titulo, autor, numeroEjemplares, numeroEjemplaresPrestados);
+        this.curso = curso;
+    }
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return "LibroTexto{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", autor='" + getAutor() + '\'' +
+                ", numeroEjemplares=" + getNumeroEjemplares() +
+                ", numeroEjemplaresPrestados=" + getNumeroEjemplaresPrestados() +
+                ", curso='" + curso + '\'' +
+                '}';
+    }
+}
